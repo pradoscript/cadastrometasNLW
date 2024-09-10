88a17e735a43e60f48dc056ba950c3f60fef16de
pradoscript <prado.joao06480@gmail.com>
2024-09-10 12:19:33 -0300

    variáveis.js

---
+++ index.js
@@ -0,0 +1,13 @@
+//hello world
+console.log("Hello World!")
+
+//variável - let
+let mensagens = "Fala, NLW POCKET!"
+console.log(mensagens)
+//variável const
+const mensagem = "Trilha JS 1"
+{
+    const mensagem = "Teste de Escopo!"
+    console.log(mensagem)
+}
+console.log(mensagem)
